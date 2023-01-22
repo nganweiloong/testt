1f6081ec49198d25d80f2b3ebd6afbfc807efe9f
Wei Loong <weiloong@netizenexperience.com>
2023-01-22 16:11:42 +0800

    hehe

---
+++ script.js
@@ -0,0 +1,190 @@
+const packets = [...document.querySelectorAll(".options h2")];
+const isDesktop = window.innerWidth > 768 && window.innerHeight > 1024;
+
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+// Lol don't cheat
+
+const hasPlayedBefore = document.cookie;
+if (document.cookie) {
+  document.body.innerHTML = "<h1>Try harder next year 🤭🤭</h1>";
+} else {
+  packets.forEach(packet =>
+    packet.addEventListener("click", () => {
+      document.cookie = "dontCheatLOL=true";
+      window.location.href =
+        packet.dataset.angpau === "1"
+          ? "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
+          : "https://onelink.tngd.my/8mmV/MONEYPACKET?p=2ed1df879a0f38e15a30cadc09d72fed423e8091413d5c5f723b866e8b852eb3b65a0c0b511df73d7ade509b06ff07df7a52953fa2c08bbc104459d1f0b209f37643a55cd5dd69b55d6ff31b29a66299";
+    })
+  );
+}
