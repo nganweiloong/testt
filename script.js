0c09f4c63d975e75ad81f5334925ba833621f6e6
Wei Loong <weiloong@netizenexperience.com>
2023-01-22 18:46:41 +0800

    random

--- script.js
+++ script.js
@@ -175,14 +175,14 @@ const isDesktop = window.innerWidth > 768 && window.innerHeight > 1024;
 // Lol don't cheat
 
 const hasPlayedBefore = document.cookie === "dontCheatLOL=true";
-
+const answer = Math.floor(Math.random() * 2 + 1);
 if (document.cookie) {
   document.body.innerHTML = "<h1>Try harder next year 🤭🤭</h1>";
 } else {
   packets.forEach(packet =>
     packet.addEventListener("click", () => {
       document.cookie = "dontCheatLOL=true";
-      if (packet.dataset.angpau === "2") {
+      if (+packet.dataset.angpau === answer) {
         window.location.href =
           "https://link.tngdigital.com.my/3BEHBtqrEVGWxUtA6";
         document.body.innerHTML = "<h1>Congratzzz 🤭🤭</h1>";
