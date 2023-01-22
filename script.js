055500aac03b503aebfb4bab6de289b6dee13abe
Wei Loong <weiloong@netizenexperience.com>
2023-01-22 19:22:24 +0800

    Revert "random"
    This reverts commit 0c09f4c63d975e75ad81f5334925ba833621f6e6.

--- script.js
+++ script.js
@@ -174,16 +174,15 @@ const isDesktop = window.innerWidth > 768 && window.innerHeight > 1024;
 // Lol don't cheat
 // Lol don't cheat
 
-const hasPlayedBefore = document.cookie === "dontCheatLOL2=true";
-const answer = Math.floor(Math.random() * 2 + 1);
-console.log(answer);
-if (hasPlayedBefore) {
+const hasPlayedBefore = document.cookie === "dontCheatLOL=true";
+
+if (document.cookie) {
   document.body.innerHTML = "<h1>Try harder next year 🤭🤭</h1>";
 } else {
   packets.forEach(packet =>
     packet.addEventListener("click", () => {
-      document.cookie = "dontCheatLOL2=true";
-      if (+packet.dataset.angpau === answer) {
+      document.cookie = "dontCheatLOL=true";
+      if (packet.dataset.angpau === "2") {
         window.location.href =
           "https://link.tngdigital.com.my/3BEHBtqrEVGWxUtA6";
         document.body.innerHTML = "<h1>Congratzzz 🤭🤭</h1>";
